ee4afb8a33f703d9c6dd6716ab54ca8b7f751e99
teros0 <best.guess.oni@gmail.com>
2018-11-06 09:48:59 +0200

    framework for running services

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"context"
+	"cservices/ingestor"
+	"cservices/storage"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
+var (
+	port    = flag.Int("port", 3333, "http port to listen on")
+	csvPath = flag.String("path", "./resorces/data.csv", "path to the csv file")
+)
+
+func main() {
+	flag.Parse()
+
+	ctx := context.Background()
+	ctx, cancelFunc := context.WithCancel(ctx)
+
+	stop := make(chan os.Signal)
+	signal.Notify(stop, syscall.SIGTERM)
+	signal.Notify(stop, syscall.SIGKILL)
+
+	go func() {
+		select {
+		case <-stop:
+			cancelFunc()
+		case <-ctx.Done():
+		}
+	}()
+
+	go func() {
+		log.Println("running ingestor service")
+
+		if err := ingestor.Run(ctx, *csvPath); err != nil {
+			log.Fatalf("while running ingestor service -> %s", err)
+		}
+
+		log.Println("running storage service")
+
+		if err := storage.Run(ctx, *port); err != nil {
+			log.Fatalf("while running storage service -> %s", err)
+		}
+	}()
 }
