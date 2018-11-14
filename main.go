176617138e4fc0a0799ceda1b97a4c9bb197f1c7
teros0 <best.guess.oni@gmail.com>
2018-11-14 20:04:50 +0200

    Added WaitGroup

--- main.go
+++ main.go
@@ -5,27 +5,29 @@ import (
 	"cservices/ingestor"
 	"cservices/storage"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 )
 
 var (
 	port    = flag.String("port", ":7777", "http port to listen on")
-	csvPath = flag.String("path", "./resorces/data.csv", "path to the csv file")
+	csvPath = flag.String("path", "./resources/data.csv", "path to the csv file")
 )
 
 func main() {
 	flag.Parse()
-
+	wg := &sync.WaitGroup{}
 	ctx := context.Background()
 	ctx, cancelFunc := context.WithCancel(ctx)
 
 	stop := make(chan os.Signal)
 	signal.Notify(stop, syscall.SIGTERM)
 	signal.Notify(stop, syscall.SIGKILL)
-
+	fmt.Println("starting")
 	go func() {
 		select {
 		case <-stop:
@@ -34,17 +36,28 @@ func main() {
 		}
 	}()
 
+	wg.Add(2)
+
 	go func() {
+		defer func() {
+			wg.Done()
+		}()
 		log.Println("running storage service")
 
 		if err := (&storage.Storage{}).Run(ctx, *port); err != nil {
 			log.Fatalf("while running storage service -> %s", err)
 		}
-
+	}()
+	go func() {
+		defer func() {
+			wg.Done()
+		}()
 		log.Println("running ingestor service")
 
 		if err := (&ingestor.Ingestor{}).Run(ctx, *csvPath, *port); err != nil {
 			log.Fatalf("while running ingestor service -> %s", err)
 		}
 	}()
+	wg.Wait()
+	log.Println("finished")
 }
